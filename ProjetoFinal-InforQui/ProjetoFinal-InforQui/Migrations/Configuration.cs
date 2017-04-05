53319354dc4a92d3ad2e595bfea2cc9aeb7a9743
balbino1990 <balbino.pinto@yahoo.com>
2017-04-05 01:59:58 +0100

    descrição da base de e disponibilização da migração

---
+++ ProjetoFinal-InforQui/ProjetoFinal-InforQui/Migrations/Configuration.cs
@@ -0,0 +1,31 @@
+namespace ProjetoFinal_InforQui.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<ProjetoFinal_InforQui.Models.InforQuiDB>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(ProjetoFinal_InforQui.Models.InforQuiDB context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
--- ProjetoFinal-InforQui/ProjetoFinal-InforQui/Models/InforQuiDB.cs
+++ ProjetoFinal-InforQui/ProjetoFinal-InforQui/Models/InforQuiDB.cs
@@ -8,12 +8,16 @@ namespace ProjetoFinal_InforQui.Models
 {
     //o DbContext é a superclasse do entity
 
-    //representar a Base de Dados descrevendo as tabelas que lá estão contidas
-
-    //representar o 'construtor' desta classe
-    //identifica onde se encontra a base de dados
     public class InforQuiDB : DbContext
     {
+        //representar a Base de Dados descrevendo as tabelas que lá estão contidas
+
+        //representar o 'construtor' desta classe
+        //identifica onde se encontra a base de dados
+        public InforQuiDB() : base("InforQuiDBConnection") { }
+
+        //descrever as tabelas que estão na base de dados
+        public virtual DbSet<Utilizadores> Utilizadores { get; set; }
 
     }
 }
