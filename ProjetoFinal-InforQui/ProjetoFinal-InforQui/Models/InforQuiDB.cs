f60a93b366f825bc67e89b1d975567b3b5794789
balbino1990 <balbino.pinto@yahoo.com>
2017-04-05 01:45:32 +0100

    Criação da tabela de utilizador

---
+++ ProjetoFinal-InforQui/ProjetoFinal-InforQui/Models/InforQuiDB.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ProjetoFinal_InforQui.Models
+{
+    //o DbContext é a superclasse do entity
+
+    //representar a Base de Dados descrevendo as tabelas que lá estão contidas
+
+    //representar o 'construtor' desta classe
+    //identifica onde se encontra a base de dados
+    public class InforQuiDB : DbContext
+    {
+
+    }
+}
---
+++ ProjetoFinal-InforQui/ProjetoFinal-InforQui/Models/Utilizadores.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjetoFinal_InforQui.Models
+{
+    public class Utilizadores
+    {
+        public int UtilizadoresID { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Apelido { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public string ConfirmPassword { get; set; }
+
+        public char Genero { get; set; }
+
+        public int DataNascimento { get; set; }
+
+        public string Morada { get; set; }
+
+        public int Contacto { get; set; }
+    }
+}
