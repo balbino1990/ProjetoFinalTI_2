1535c483b23e6cf878a9507e5e6efcccf3ef4ec0
balbino1990 <balbino.pinto@yahoo.com>
2017-04-11 03:50:29 +0100

    Criação do Controller e Views do Home (mais imagens para o controller home)

--- ProjetoFinal-InforQui/ProjetoFinal-InforQui/Controllers/HomeController.cs
+++ ProjetoFinal-InforQui/ProjetoFinal-InforQui/Controllers/HomeController.cs
@@ -1,20 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using ProjetoFinal_InforQui.Models;
 
 namespace ProjetoFinal_InforQui.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: Home
+        // GET: Home/Index
         public ActionResult Index()
         {
             return View();
         }
 
-        //POST: Home
+        //GET: Home/Utilizador
+        public ActionResult Utilizador()
+        {
+            return View();
+        }
+
+        //GET: Home/Sobre
+        public ActionResult Sobre()
+        {
+            return View();
+        }
+
+        //GET: Home/Contactos
+        public ActionResult Contactos()
+        {
+            return View();
+        }
         
     }
 }
